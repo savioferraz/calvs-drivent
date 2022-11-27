60d5de3e3bc46178a677a11425f46aab32211f96
savioferraz <savio.ferraz88@gmail.com>
2022-11-27 04:47:10 -0300

    test: add lodge routes tests

--- src/services/lodge-service/index.ts
+++ src/services/lodge-service/index.ts
@@ -1,7 +1,6 @@
 import { notFoundError, requestError } from "@/errors";
 import enrollmentRepository from "@/repositories/enrollment-repository";
 import lodgeRepository from "@/repositories/lodge-repository";
-import paymentRepository from "@/repositories/payment-repository";
 import ticketRepository from "@/repositories/ticket-repository";
 
 async function listHotels(userId: number, hotelId: number) {
@@ -11,9 +10,6 @@ async function listHotels(userId: number, hotelId: number) {
   const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
   if (!ticket || ticket.TicketType.includesHotel === false) throw requestError(403, "ForbiddenError");
   if (ticket.status !== "PAID") throw requestError(402, "PaymentRequired");
-
-  // const payment = await paymentRepository.findPaymentByTicketId(ticket.id);
-  // if (!payment) throw requestError(403, "ForbiddenError");
 
   if (hotelId) {
     const hotelRooms = await lodgeRepository.findHotelWithRooms(hotelId);
@@ -27,22 +23,6 @@ async function listHotels(userId: number, hotelId: number) {
     return hotels;
   }
 }
-
-// async function listRooms(userId: number, hotelId: number) {
-//   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
-//   if (!enrollment) throw notFoundError();
-
-//   const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
-//   if (!ticket || ticket.TicketType.includesHotel === false || ticket.status !== "PAID") throw notFoundError();
-
-//   const payment = await paymentRepository.findPaymentByTicketId(ticket.id);
-//   if (!payment) throw notFoundError();
-
-//   const hotelRooms = await lodgeRepository.findHotelWithRooms(hotelId);
-//   if (!hotelRooms) throw notFoundError();
-
-//   return hotelRooms;
-// }
 
 const lodgeService = { listHotels };
 
